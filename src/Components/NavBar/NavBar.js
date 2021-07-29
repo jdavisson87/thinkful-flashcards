f649e23ac67c02a22725e21cd64dada8b90b8be2
Kittaka <jdavisson87@gmail.com>
2021-07-29 14:34:32 -0500

    removed navbar from createdeck component and added list directly into the component. Will try to create a navbar component later.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,22 +1,7 @@
 import React from 'react';
-import { useHistory, useRouteMatch, Link } from 'react-router-dom';
 
 const NavBar = () => {
-  const history = useHistory();
-  const url = useRouteMatch();
-
-  console.log(url);
-  return (
-    <nav aria-label="breadcrumb">
-      <ol className="breadcrumb">
-        <Link to="/">
-          <li className="breadcrumb-item active" aria-current="page">
-            Home
-          </li>
-        </Link>
-      </ol>
-    </nav>
-  );
+  return <div>NavBar</div>;
 };
 
 export default NavBar;
--- src/Containers/CreateDeck/CreateDeck.js
+++ src/Containers/CreateDeck/CreateDeck.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CreateDeckForm from '../../Forms/CreateDeckForm';
 
 const CreateDeck = () => {
   return (
     <div>
-      <p>Create Deck page</p>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item" key="home-link">
+            <Link to={'/'}>
+              <i className="bi bi-house-door-fill" />
+              Home
+            </Link>
+          </li>
+          <li
+            className="breadcrumb-item active"
+            aria-current="page"
+            key="create-deck-link"
+          >
+            Create Deck
+          </li>
+        </ol>
+      </nav>
+      <CreateDeckForm />
     </div>
   );
 };
