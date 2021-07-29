718cc8db749ac6597a93be9cb7c8e86883fe89d6
Kittaka <jdavisson87@gmail.com>
2021-07-29 14:56:09 -0500

    created change handler function and submit function for the deck creation

--- src/Forms/CreateDeckForm.js
+++ src/Forms/CreateDeckForm.js
@@ -1,30 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { createDeck } from '../utils/api/index';
 
 const CreateDeckForm = () => {
+  const formDefault = {
+    name: '',
+    description: '',
+  };
+
+  const [newDeck, setNewDeck] = useState(formDefault);
+
+  const history = useHistory();
+  const cancelClick = () => {
+    history.push('/');
+  };
+
+  const onChange = (event) => {
+    setNewDeck({
+      ...newDeck,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const response = await createDeck(newDeck);
+    history.push(`/decks/${response.id}`);
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Create Deck</h1>
       <div className="form-group">
-        <label for="deck-form-name">Name</label>
+        <label htmlFor="deck-form-name">Name</label>
         <input
-          id="deckName"
+          id="name"
           type="text"
           className="form-control"
           placeholder="Deck Name"
+          onChange={onChange}
         />
       </div>
       <div className="form-group">
-        <label for="deck-form-description">Description</label>
+        <label htmlFor="deck-form-description">Description</label>
         <textarea
+          id="description"
           type="text"
           className="form-control"
           placeholder="Brief description of the deck"
           rows="5"
+          onChange={onChange}
         />
       </div>
       <div>
-        <button className="btn btn-secondary mr-2">Cancel</button>
-        <button className="btn btn-primary">Submit</button>
+        <button
+          onClick={() => cancelClick()}
+          className="btn btn-secondary mr-2"
+        >
+          Cancel
+        </button>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </div>
     </form>
   );
