cf554d6e9d69354f1fd796b5ba1c12e4cbe26275
Kittaka <jdavisson87@gmail.com>
2021-07-30 13:19:44 -0500

    Moving routes/switches around for nesting routes

--- src/Containers/Decks/Decks.js
+++ src/Containers/Decks/Decks.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, Route, Switch, useHistory } from 'react-router-dom';
 import DeckPreview from '../../Components/DeckPreview/DeckPreview';
+import Deck from '../../Containers/Deck/Deck';
+import CreateDeck from '../CreateDeck/CreateDeck';
+import { listDecks, deleteDeck } from '../../utils/api/index';
 
-const Decks = ({ decks, deckDelete }) => {
+const Decks = () => {
+  const [decks, setDecks] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function getDecks() {
+      const response = await listDecks();
+      setDecks(response);
+    }
+    getDecks();
+  }, []);
+
+  const deckDeleteHandler = (id) => {
+    if (window.confirm('Delete this deck?')) {
+      deleteDeck(id);
+      setDecks((current) => current.filter((deck) => deck.id !== id));
+      history.push('/');
+    }
+  };
   let content =
     decks.length > 0 ? (
       decks.map((deck) => (
@@ -10,7 +31,7 @@ const Decks = ({ decks, deckDelete }) => {
           name={deck.name}
           description={deck.description}
           deckId={deck.id}
-          onDelete={deckDelete}
+          onDelete={deckDeleteHandler}
           key={`${deck.name}${deck.id}`}
         />
       ))
@@ -22,7 +43,17 @@ const Decks = ({ decks, deckDelete }) => {
       <Link to={`/decks/new`} className="btn btn-secondary">
         <i className="bi bi-plus-lg"></i> {` `}Create Deck
       </Link>
-      <ul className="mt-1 p-0">{content}</ul>
+      <Switch>
+        <Route exact path={'/decks/:deckId'}>
+          <Deck />
+        </Route>
+        <Route path={'/decks/new'}>
+          <CreateDeck />
+        </Route>
+        <Route>
+          <ul className="mt-1 p-0">{content}</ul>
+        </Route>
+      </Switch>
     </div>
   );
 };
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,45 +1,17 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { Switch, Route, useHistory } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import Decks from '../Containers/Decks/Decks';
-import CreateDeck from '../Containers/CreateDeck/CreateDeck';
-import Deck from '../Containers/Deck/Deck';
 import NotFound from './NotFound';
-import { listDecks, deleteDeck } from '../utils/api/index';
 
 function Layout() {
-  const [decks, setDecks] = useState([]);
-  const history = useHistory();
-
-  useEffect(() => {
-    async function getDecks() {
-      const response = await listDecks();
-      setDecks(response);
-    }
-    getDecks();
-  }, []);
-
-  const deckDeleteHandler = (id) => {
-    if (window.confirm('Delete this deck?')) {
-      deleteDeck(id);
-      setDecks((current) => current.filter((deck) => deck.id !== id));
-      history.push('/');
-    }
-  };
-
   return (
     <Fragment>
       <Header />
       <div className="container">
         <Switch>
-          <Route exact path="/">
-            <Decks decks={decks} deckDelete={deckDeleteHandler} />
-          </Route>
-          <Route path={`/decks/new`}>
-            <CreateDeck />
-          </Route>
-          <Route path={'/decks/:deckId'}>
-            <Deck deckDelete={deckDeleteHandler} />
+          <Route path="/">
+            <Decks />
           </Route>
           <Route>
             <NotFound />
